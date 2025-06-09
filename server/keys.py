68c81a5d839661a0d64523d7f3dbf9509eb7da71
Giulia Tortorella <giulia.tortorella@outlook.it>
2025-06-09 18:45:15 +0200

    Update keys.py

--- server/keys.py
+++ server/keys.py
@@ -1,7 +1,7 @@
 import os
 
-# keys GT.py
+# placeholder "keys.py" - Subvstitute with your own file
 
-OPENAI_API_KEY = "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"  # Example key
-CLOUDFLARE_API_KEY = "aff7682366b0ea5a4393acfbf86318fde3e08"  # key
-CLOUDFLARE_ACCOUNT_ID = "e80ef4eafb95b6d10d98454c892cc4b6"  # account ID
+OPENAI_API_KEY = ""  # AI key
+CLOUDFLARE_API_KEY = ""  # CF key
+CLOUDFLARE_ACCOUNT_ID = ""  # CF account ID
